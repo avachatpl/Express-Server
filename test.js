3dced525d38c76cc8be260d32c0dc1f5bc94ca92
unknown <avachatpriya@.com>
2020-02-05 14:54:49 +0530

    -a

--- test.js
+++ test.js
@@ -7,12 +7,19 @@ var port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 //app.get("/hello",(req,res)=>res.send("hello word"));
+const bodyParser= require('body-parser');
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 //app.listen(3000,() => console.log('aksfjeiofjsdofjcksdf'));
 app.all("/",(req,res)=>{
 	//console.log("name===="+req.query.name);
 	//console.log("name===="+ JSON.stringify(req.body));
+	console.log(req.body.a);
 var person={
 	
 		"name":"priya",
